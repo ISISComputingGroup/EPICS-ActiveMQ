3ac6973fe8c49cd58163c577edb71e6880261e5d
Dominic Oram <dominic.oram@stfc.ac.uk>
2021-07-27 10:24:11 +0100

    Changed process for updating activemq

--- start-jms-server.bat
+++ start-jms-server.bat
@@ -11,6 +11,6 @@ set ACTIVEMQ_DATA=%ICPVARDIR%\activemq
 set ACTIVEMQ_TMP=%ICPVARDIR%\tmp
 set ACTIVEMQ_OPTS=-Xms64m -Xmx128m -Djava.util.logging.config.file=logging.properties -Djava.security.auth.login.config="%ACTIVEMQ_CONF%\login.config"
 
-call "%MYDIRBLOCK%apache-activemq-5.16.2\bin\activemq.bat" start
+call "%MYDIRBLOCK%apache-activemq\bin\activemq.bat" start
 
 exit
